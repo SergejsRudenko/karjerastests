2a52ca2342ff1675061f27a5b5a2d4e3912e3222
Sergejs <sergejs_rudenko@inbox.lv>
2022-03-02 13:56:32 +0200

    part one works

--- src/js/modules/quiz.js
+++ src/js/modules/quiz.js
@@ -7,99 +7,152 @@ export function quiz() {
             questionOne: {
                 "questionOne": "Man patīk pētniecisks darbs",
                 "QOID": `Q1Q1`,
-                "questionOneAnswer": false,
+                "questionOneAnswer": '',
                 "questionTwo": "Man piemīt pacietība un neatlaidība ilgstoši strādāt pie vienas problēmas risinājuma",
                 "QTID": `Q1Q2`,
-                "questionTwoAnswer": false,
-                "fallbackQuestions": ['BF(All)','ĶF(All)','MF(All)','ĢZZF(All)','HZF(KS)','PPMF(STS)','SZF(S)'],
+                "questionTwoAnswer": '',
+                "fallbackQuestions": ['A'],
                 "ID": 1
+                // ['BF(All)','ĶF(All)','MF(All)','ĢZZF(All)','HZF(KS)','PPMF(STS)','SZF(S)']
             },
-            // questionTwo: {
-            //     "questionOne": "Question",
-            //     "QOID": "Q2Q1",
-            //     "questionOneAnswer": false,
-            //     "questionTwo": "Question 2",
-            //     "QTID": "Q2Q2",
-            //     "questionTwoAnswer": false,
-            //     "fallbackQuestions": ['ES','RTU'],
-            //     "ID": 3
-            // },
-            // questionThree: {
-            //     "questionOne": "Question",
-            //     "QOID": "Q3Q1",
-            //     "questionOneAnswer": false,
-            //     "questionTwo": "Question 2",
-            //     "QTID": "Q3Q2",
-            //     "questionTwoAnswer": false,
-            //     "fallbackQuestions": ['MRI','OST'],
-            //     "ID": 5
-            // }
+            questionTwo: {
+                "questionOne": "Question",
+                "QOID": "Q2Q1",
+                "questionOneAnswer": '',
+                "questionTwo": "Question 2",
+                "QTID": "Q2Q2",
+                "questionTwoAnswer": '',
+                "fallbackQuestions": ['B'],
+                "ID": 3
+            },
+            questionThree: {
+                "questionOne": "Question",
+                "QOID": "Q3Q1",
+                "questionOneAnswer": '',
+                "questionTwo": "Question 2",
+                "QTID": "Q3Q2",
+                "questionTwoAnswer": '',
+                "fallbackQuestions": ['C'],
+                "ID": 5
+            },
+            questionFour: {
+                "questionOne": "Question",
+                "QOID": "Q4Q1",
+                "questionOneAnswer": '',
+                "questionTwo": "Question 2",
+                "QTID": "Q5Q2",
+                "questionTwoAnswer": '',
+                "fallbackQuestions": ['D'],
+                "ID": 7
+            }
         };
-    let fallbackAnswers = [];
-    let fallBackQuestions = {};
-    function addElement () {
+    let fallbackAnswers = ['D'];
+    let followQuestions = {
+        "BF(ALL)": {
+            "questionOne" : "1",
+            "questionTwo" : "2",
+            "questionThree" : "3",
+            "questionFour" : "4",
+            "questionFive" : "5",
+            "questionSix" : "6",
+        }
+    };
+    function createPartOne () {
+        let currentDiv = document.querySelector(".questions__form");
         for (const item of Object.values(mainQuestions)) {
             console.log(item);
             let questionOne = item.questionOne;
                 let questionTwo = item.questionTwo;
-                let questionOneAnswer = item.questionOneAnswer;
-                let questionTwoAnswer = item.questionTwoAnswer;
                 let ID = item.ID;
                 let ID1 = item.QOID;
                 let ID2 = item.QTID;
                 let newDiv = document.createElement("div");
                 newDiv.innerHTML = `<p class="question">${questionOne}</p>
                 <div class="answers">
-                    <input type="radio" value="yes" id="answerYes${ID}" name="question${ID}">
+                    <input type="radio" value="true" id="answerYes${ID}" name="question${ID}">
                     <label for="answerYes${ID}">YES</label>
-                    <input type="radio" value="no" id="answerNo${ID}" name="question${ID}">
+                    <input type="radio" value="false" id="answerNo${ID}" name="question${ID}">
                     <label for="answerNo${ID}">NO</label>
                 </div>
                 <p class="question">${questionTwo}</p>
                 <div class="answers">
-                    <input type="radio" value="yes" id="answerYes${ID}" name="question${ID+1}">
+                    <input type="radio" value="true" id="answerYes${ID}" name="question${ID+1}">
                     <label for="answerYes${ID}">YES</label>
-                    <input type="radio" value="no" id="answerNo${ID}" name="question${ID+1}">
+                    <input type="radio" value="false" id="answerNo${ID}" name="question${ID+1}">
                     <label for="answerNo${ID}">NO</label>
                 </div>`;
                 newDiv.classList.add('questions');
-                let currentDiv = document.querySelector(".questions__form");
-                currentDiv.insertAdjacentElement('afterbegin', newDiv);
+                currentDiv.insertAdjacentElement('beforeend', newDiv);
           }
-        
+          let newButton = document.createElement("button");
+          newButton.classList.add('submit');
+          newButton.setAttribute('type', 'submit');
+          newButton.innerText = 'Submit';
+          currentDiv.insertAdjacentElement('beforeend', newButton);
     }
-    function writeAnswers() {
-        for (const item of Object.values(mainQuestions)) {
-            item.questionOneAnswer = true;
-            item.questionTwoAnswer = true;
-          }
-    }
+    // function createPartTwo () {
+    //     let currentDiv = document.querySelector(".questions__formTwo");
+    //     for (const item of Object.values(followQuestions)) {
+    //         console.log(item);
+    //             let questionOne = item.questionOne;
+    //             let questionTwo = item.questionTwo;
+    //             let ID = item.ID;
+    //             let ID1 = item.QOID;
+    //             let ID2 = item.QTID;
+    //             let newDiv = document.createElement("div");
+    //             newDiv.innerHTML = `<p class="question">${questionOne}</p>
+    //             <div class="answers">
+    //                 <input type="radio" value="true" id="answerYes" name="question">
+    //                 <label for="answerYes">YES</label>
+    //                 <input type="radio" value="false" id="answerNo" name="question">
+    //                 <label for="answerNo">NO</label>
+    //             </div>
+    //             <p class="question">${questionTwo}</p>
+    //             <div class="answers">
+    //                 <input type="radio" value="true" id="answerYes" name="question">
+    //                 <label for="answerYes">YES</label>
+    //                 <input type="radio" value="false" id="answerNo" name="question">
+    //                 <label for="answerNo">NO</label>
+    //             </div>`;
+    //             newDiv.classList.add('questions');
+    //             currentDiv.insertAdjacentElement('beforeend', newDiv);
+    //       }
+    // }
+  
     function createFallBackAnswers() {
-        
-        for (const item of Object.entries(mainQuestions)) {
-            for (let i = 0; i < item.length; i++) {
-                const optionOne = item[i].questionOneAnswer;
-                const optionTwo = item[i].questionTwoAnswer;
-                if(optionOne && optionTwo) {
-                    fallbackAnswers.push(...item[i].fallbackQuestions);
-                }
+            // Check if both questions are true
+        for (const item of Object.values(mainQuestions)) {
+            if (item.questionOneAnswer === 'true' && item.questionTwoAnswer === 'true') {
+                fallbackAnswers.push(...item.fallbackQuestions);
             }
           }
-          return fallbackAnswers;
     }
     
     form.addEventListener("submit", function(event) {
         var data = new FormData(form);
         var output = "";
+        let counter = 1;
+
         for (const entry of data) {
-          output = output + entry[0] + "=" + entry[1] + "\r";
-        };
-        log.innerText = output;
+            output = output + entry[0] + "=" + entry[1] + "\r";
+          };
+          log.innerText = output;
+          //Writes down the annswer values
+        for (const item of Object.values(mainQuestions)) {
+            item.questionOneAnswer = data.get(`question${counter}`);
+            console.log(item.questionOneAnswer);
+            item.questionTwoAnswer = data.get(`question${counter+1}`);
+            console.log(item.questionTwoAnswer);
+            counter +=2;
+            console.log(counter);
+        }
+        createFallBackAnswers();
+        console.log(fallbackAnswers);
+        console.log(mainQuestions.questionThree);
+
         event.preventDefault();
       }, false);
 
-    addElement();
-    writeAnswers();
-    createFallBackAnswers();
-    console.log(fallbackAnswers);
+    createPartOne();
+    // writeAnswers();
 }
