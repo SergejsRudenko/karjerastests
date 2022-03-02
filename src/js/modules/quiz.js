bac952c0ba301e9efbbe3e338bf1aa42678150e1
Sergejs <sergejs_rudenko@inbox.lv>
2022-03-02 14:38:29 +0200

    part two testing create function

--- src/js/modules/quiz.js
+++ src/js/modules/quiz.js
@@ -55,12 +55,22 @@ export function quiz() {
             "questionFour" : "4",
             "questionFive" : "5",
             "questionSix" : "6",
+            "ID": 1
+        },
+        "LU": {
+            "questionOne" : "1",
+            "questionTwo" : "2",
+            "questionThree" : "3",
+            "questionFour" : "4",
+            "questionFive" : "5",
+            "questionSix" : "6",
+            "ID": 3
         }
     };
     function createPartOne () {
         let currentDiv = document.querySelector(".questions__form");
         for (const item of Object.values(mainQuestions)) {
-            console.log(item);
+            // console.log(item);
             let questionOne = item.questionOne;
                 let questionTwo = item.questionTwo;
                 let ID = item.ID;
@@ -90,34 +100,47 @@ export function quiz() {
           newButton.innerText = 'Submit';
           currentDiv.insertAdjacentElement('beforeend', newButton);
     }
-    // function createPartTwo () {
-    //     let currentDiv = document.querySelector(".questions__formTwo");
-    //     for (const item of Object.values(followQuestions)) {
-    //         console.log(item);
-    //             let questionOne = item.questionOne;
-    //             let questionTwo = item.questionTwo;
-    //             let ID = item.ID;
-    //             let ID1 = item.QOID;
-    //             let ID2 = item.QTID;
-    //             let newDiv = document.createElement("div");
-    //             newDiv.innerHTML = `<p class="question">${questionOne}</p>
-    //             <div class="answers">
-    //                 <input type="radio" value="true" id="answerYes" name="question">
-    //                 <label for="answerYes">YES</label>
-    //                 <input type="radio" value="false" id="answerNo" name="question">
-    //                 <label for="answerNo">NO</label>
-    //             </div>
-    //             <p class="question">${questionTwo}</p>
-    //             <div class="answers">
-    //                 <input type="radio" value="true" id="answerYes" name="question">
-    //                 <label for="answerYes">YES</label>
-    //                 <input type="radio" value="false" id="answerNo" name="question">
-    //                 <label for="answerNo">NO</label>
-    //             </div>`;
-    //             newDiv.classList.add('questions');
-    //             currentDiv.insertAdjacentElement('beforeend', newDiv);
-    //       }
-    // }
+    function createPartTwo () {
+        let currentDiv = document.querySelector(".questions__formTwo");
+        //Removes questions that are not in fallbackQuestions
+        console.log(Object.keys(followQuestions));
+        // for (let item of Object.keys(followQuestions)){
+        //     console.log(item);
+        //     if (!fallbackAnswers.includes(item)) {
+        //         delete followQuestions[item];
+        //     }
+        // }
+        console.log(followQuestions);
+        for (const item of Object.values(followQuestions)) {
+                let questionOne = item.questionOne;
+                let questionTwo = item.questionTwo;
+                let ID = item.ID;
+                let ID1 = item.QOID;
+                let ID2 = item.QTID;
+                let newDiv = document.createElement("div");
+                newDiv.innerHTML = `<p class="question">${questionOne}</p>
+                <div class="answers">
+                    <input type="radio" value="true" id="answerYes${ID}" name="question${ID}">
+                    <label for="answerYes${ID}">YES</label>
+                    <input type="radio" value="false" id="answerNo${ID}" name="question${ID}">
+                    <label for="answerNo${ID}">NO</label>
+                </div>
+                <p class="question">${questionTwo}</p>
+                <div class="answers">
+                    <input type="radio" value="true" id="answerYes${ID}" name="question${ID+1}">
+                    <label for="answerYes${ID}">YES</label>
+                    <input type="radio" value="false" id="answerNo${ID}" name="question${ID+1}">
+                    <label for="answerNo${ID}">NO</label>
+                </div>`;
+                newDiv.classList.add('questions');
+                currentDiv.insertAdjacentElement('beforeend', newDiv);
+          }
+          let newButton = document.createElement("button");
+          newButton.classList.add('submit');
+          newButton.setAttribute('type', 'submit');
+          newButton.innerText = 'Submit';
+          currentDiv.insertAdjacentElement('beforeend', newButton);
+    }
   
     function createFallBackAnswers() {
             // Check if both questions are true
@@ -140,19 +163,20 @@ export function quiz() {
           //Writes down the annswer values
         for (const item of Object.values(mainQuestions)) {
             item.questionOneAnswer = data.get(`question${counter}`);
-            console.log(item.questionOneAnswer);
+            // console.log(item.questionOneAnswer);
             item.questionTwoAnswer = data.get(`question${counter+1}`);
-            console.log(item.questionTwoAnswer);
+            // console.log(item.questionTwoAnswer);
             counter +=2;
-            console.log(counter);
+            // console.log(counter);
         }
         createFallBackAnswers();
-        console.log(fallbackAnswers);
-        console.log(mainQuestions.questionThree);
+        // console.log(fallbackAnswers);
+        // console.log(mainQuestions.questionThree);
 
         event.preventDefault();
       }, false);
 
     createPartOne();
+    createPartTwo();
     // writeAnswers();
 }
